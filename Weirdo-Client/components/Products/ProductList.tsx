4d9b7cb46db773c977dc4d30a09895b8df10b17f
Junfeng Ou <junfengou@gmail.com>
2023-06-19 17:19:59 -0500

    Fixed a small style issue

--- Weirdo-Client/components/Products/ProductList.tsx
+++ Weirdo-Client/components/Products/ProductList.tsx
@@ -24,14 +24,15 @@ const ProductList: React.FC = () => {
         </Link>
       </div>
 
-      <div className='grid grid-cols-3 gap-4 content-normal md:grid-cols-2'>
+{/* md:grid-cols-2 sm:border-2 border-cyan-400 */}
+      <div className='grid grid-cols-2 gap-4 content-normal'>
         {productListRecoilState?.products?.map(item => (
           <Link key={item.id} href={`/products/${item.id}`} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
               <Image width={100} height={100}  loading='lazy' className="object-cover w-full rounded-t-lg h-60 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={item.imagePath} alt="image" />
               <div className="flex flex-col justify-between p-4 leading-normal">
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item?.name}</h5>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item?.description}</p>
-                  <div className="rounded-r-lg">{"$"+item.price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</div>
+                  <div>{"$"+item.price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</div>
               </div>
           </Link>
         ))}
