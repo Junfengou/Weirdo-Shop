1d746875f1dc2cae1d0db81632795a7ac82f40a7
Junfeng Ou <junfengou@gmail.com>
2023-06-19 20:44:47 -0500

    Second deployment test

--- Weirdo-Client/components/Products/Product.tsx
+++ Weirdo-Client/components/Products/Product.tsx
@@ -19,12 +19,12 @@ const Product = () => {
   return (
     <div className='flex flex-row mt-4 mr-10 h-96 items-center justify-around content-center'>
       <div>
-        <Image src={product.imagePath} height={370} width={370} alt="productImage" />
+        <Image src={product?.imagePath} height={370} width={370} alt="productImage" />
       </div>
 
       <div className='flex flex-col bg-slate-200 rounded-md h-96 w-80 ml-4 items-left '>
         <div className='h-64 flex flex-col justify-evenly'>
-          <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white ml-4'>{product.name}</h5>
+          <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white ml-4'>{product?.name}</h5>
           <p className='ml-4 mr-4'>{product?.description}</p>
           <p className='text-1xl font-bold tracking-tight ml-4 mr-4'>{transformToDollar(product?.price)}</p>
         </div>
