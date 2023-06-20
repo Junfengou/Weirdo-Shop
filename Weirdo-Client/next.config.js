71d729c2476722dd5ee73894655e2f012321a5f2
Junfeng Ou <junfengou@gmail.com>
2023-06-19 20:23:18 -0500

    Added paths to next.config

--- Weirdo-Client/next.config.js
+++ Weirdo-Client/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/products/products': { page: '/products/products' },
+      '/products/addproduct': { page: '/products/addproduct' },
+      '/products/[id]': { page: '/products/[id]' },
+      // Add other pages here
+    };
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
