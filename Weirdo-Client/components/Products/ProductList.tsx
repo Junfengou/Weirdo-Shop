98299143192fa3ae7d706468c277b37fd8f7b10f
Junfeng Ou <junfengou@gmail.com>
2023-06-27 08:20:24 -0500

    Fixed style issue with product list

--- Weirdo-Client/components/Products/ProductList.tsx
+++ Weirdo-Client/components/Products/ProductList.tsx
@@ -25,7 +25,7 @@ const ProductList: React.FC = () => {
       </div>
 
 {/* md:grid-cols-2 sm:border-2 border-cyan-400 */}
-      <div className='grid grid-cols-2 gap-4 content-normal ml-5 product-list-view-mobile-l:grid-cols-2 product-list-view-mobile-m:grid-cols-1'>
+      <div className='grid grid-cols-2 gap-4 content-normal ml-5 product-list-view-mobile-l:grid-cols-2 product-list-view-mobile-m:grid-cols-1 product-list-view-tablet-l:grid-cols-1'>
         {productListRecoilState?.products?.map(item => (
           <Link key={item?.id} href={`/products/${item?.id}`} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 sm:ml-6">
               {item?.imagePath && <Image width={100} height={100}  loading='lazy' className="object-cover w-full rounded-t-lg h-60 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={item?.imagePath} alt="image" /> }
